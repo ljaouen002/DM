7b99bffe12dba1f7c2e547f955414a5a851b4aca
cpelletie005 <57910977+cpelletie005@users.noreply.github.com>
2019-11-28 16:17:14 +0100

    Update MethodeRes.h

--- MethodeRes.h
+++ MethodeRes.h
@@ -7,14 +7,11 @@
 class MethodeRes
 {
   protected:
-    Eigen::SparseMatrix<double> _A;
-    Eigen::SparseVector<double> _b;
-    Eigen::SparseVector<double> _r;
-    //int _N; //taille de la matrice
+    Eigen::MatrixXd _A, _b;
+    // Vecteur initial et vecteur solution
+    Eigen::VectorXd _sol0, _sol, _r;
     // Écriture du fichier
     std::ofstream _file_out;
-    // Vecteur initial et vecteur solution
-    Eigen::VectorXd _sol0, _sol;
     MethodeRes* _methode;
   public:
     // Constructeur par défaut
@@ -24,9 +21,9 @@ class MethodeRes
     // Initialiser le nom du fichier solution
     void InitializeFileName(const std::string file_name);
     // Initialisation
-    virtual void Initialisation(int N, Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A, Eigen::SparseVector<double> sol0)=0; //, std::string results, MethodeRes* methode)=0;
+    void Initialisation(Eigen::VectorXd b, Eigen::MatrixXd A, Eigen::VectorXd sol0 , Eigen::VectorXd r, std::string results, MethodeRes* methode);
     // Calcul de x
-    virtual void calcul_sol(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A) =0;
+    virtual void calcul_sol() =0;
     // Remplissage de la solution
     void SaveSolution(const int nb_iterations);
 };
@@ -35,32 +32,10 @@ class MethodeRes
 class Jacobi : public MethodeRes
 {
   private:
-    //Eigen::SparseVector<double> _r;
-    //Eigen::SparseVector<double> _sol; //Lisa: Pour moi sol est déjà déclaré dans la classe mère
-    Eigen::SparseMatrix<double> _M, _N, _D, _E, _F;
-
+    Eigen::MatrixXd _D, _E, _F, _M, _N;
   public:
-    //constructeur
-    Jacobi(); //Pour moi il faudrait définir M, N, D, E et F dans le constructeur
-    void Initialisation(int N, Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A, Eigen::SparseVector<double> sol0);
-    void calcul_sol(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A);
-};
-
-
-
-// Classe fille publique de MethodeRes
-class Residu : public MethodeRes
-{
-  private:
-    double _alpha; //coefficient de descente
-    //Eigen::SparseVector<double> _r; // direction de descente, comme méthode de gradient, r=d
-    //Eigen::SparseMatrix<double> _M; //Matrice préconditionnement, dans question 3
-
-  public:
-    //constructeur
-    Residu(double alpha);
-    void Initialisation(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A, Eigen::SparseVector<double> sol0);
-    void calcul_sol(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A);
+    Jacobi();
+    void calcul_sol();
 };
 
 // Classe fille publique de MethodeRes
@@ -68,91 +43,39 @@ class GPO : public MethodeRes
 {
   private:
     double _alpha; //coefficient de descente
-    //Eigen::SparseVector<double> _r; // direction de descente, comme méthode de gradient, r=d
+    Eigen::VectorXd _z;
     //Eigen::SparseMatrix<double> _M; //Matrice préconditionnement, dans question 3
-
   public:
     //constructeur
-    GPO(double alpha);
-    void Initialisation(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A, Eigen::SparseVector<double> sol0);
-    void calcul_sol(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A);
+    GPO();
+    void calcul_sol();
+};
+
+// Classe fille publique de MethodeRes
+class Residu : public MethodeRes
+{
+  private:
+    double _alpha; //coefficient de descente
+    Eigen::VectorXd _z;
+    //Eigen::SparseMatrix<double> _M; //Matrice préconditionnement, dans question 3
+  public:
+    Residu();
+    void calcul_sol();
 };
 
 
+/*
+// Classe fille publique de MethodeRes
 class GMRes : public MethodeRes
 {
   private:
     double _beta;
-
-    //Eigen::SparseVector<double> _r; // direction de descente, comme méthode de gradient, r=d
-    //Eigen::SparseMatrix<double> _H;
-    //Eigen::SparseMatrix<double> _v_arno; //Matrice préconditionnement, dans question 3
-
   public:
     //constructeur
-    GMRes(double beta);
-    void Initialisation(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A, Eigen::SparseVector<double> sol0, double beta);
-    void Arnoldi(Eigen::SparseVector<double> v, int N, Eigen::SparseMatrix<double> A, Eigen::SparseMatrix<double> v_arno, SparseMatrix<double>  H);
+    GMRes(double beta, Eigen::SparseVector<double> r);
+    void Initialisation(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A, Eigen::SparseVector<double> sol0);
+    void Arnoldi(Eigen::SparseVector<double> v, int N, Eigen::SparseMatrix<double> A, Eigen::SparseMatrix<double> v_arno, SparseMatrix<double> H);
     void calcul_sol(Eigen::SparseVector<double> b, Eigen::SparseMatrix<double> A);
-};
-
-
-
-/*
-// Classe fille publique d’OdeSystem
-class SecondExampleOdeSystem : public OdeSystem
-{
-  public:
-    void BuildF(const double t, const Eigen::VectorXd & sol); //f(X,t) = (-y,x)
-};
-
-// Classe fille publique d’OdeSystem
-class ThirdExampleOdeSystem : public OdeSystem
-{
-  public:
-    void BuildF(const double t, const Eigen::VectorXd & sol); //f(X,t) = tX²
-};
-
-// Classe fille publique d’OdeSystem
-class LotkaVolterraOdeSystem : public OdeSystem
-{
-  private:
-    //Paramètre Lotka Volterra
-    double _a;
-    double _b;
-    double _c;
-    double _d;
-  public:
-    LotkaVolterraOdeSystem(double a, double b, double c, double d);
-    void BuildF(const double t, const Eigen::VectorXd & sol); //f(X,t) = (x(a-by),y(cx-d))
-};
-
-// Classe fille publique d’OdeSystem
-class PendulumOdeSystem : public OdeSystem
-{
-  private:
-    //Paramètre Lotka Volterra
-    double _l;
-    double _m;
-    double _k;
-  public:
-    PendulumOdeSystem(double l, double m); //_k=0
-    PendulumOdeSystem(double l, double m, double k);
-    void BuildF(const double t, const Eigen::VectorXd & sol); //f(X,t) = (y,-g/l*sin(x)-k/ml²*y)
-    void SaveSolution(const double t, const Eigen::VectorXd & sol);
-};
-
-// Classe fille publique d’OdeSystem
-class CircuitRLCOdeSystem : public OdeSystem
-{
-  private:
-    double _R;
-    double _C;
-    double _q0;
-  public:
-    CircuitRLCOdeSystem(double R, double C, double q0);
-    void BuildF(const double t, const Eigen::VectorXd & sol); //f(X,t) = -(1/RC)*X
-    void SaveSolution(const double t, const Eigen::VectorXd & sol);
 };
 */
 #define _METHODE_RES_H
